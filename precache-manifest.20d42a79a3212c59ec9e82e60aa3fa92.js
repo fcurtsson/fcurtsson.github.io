3fe39c18a2fdfe8cf49eb85893dc23a98635eed2
Fanny Curtsson <curtsson@kth.se>
2020-04-24 01:29:40 +0200

    Updates

---
+++ precache-manifest.20d42a79a3212c59ec9e82e60aa3fa92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5990bd7639369555dff000e4fd9957c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aed69fafa24c58d51710",
+    "url": "/static/css/main.0df4d56a.chunk.css"
+  },
+  {
+    "revision": "a9f7ee1e3721e9e11de8",
+    "url": "/static/js/2.445367ca.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.445367ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed69fafa24c58d51710",
+    "url": "/static/js/main.dc7dc153.chunk.js"
+  },
+  {
+    "revision": "4567734cd469a9475f6c",
+    "url": "/static/js/runtime-main.736723a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
