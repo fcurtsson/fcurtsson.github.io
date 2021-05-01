fef9f11b3b5b1d33a844fae658d3d6a4d01c6531
fcurtsson <fanny@curtsson.com>
2021-05-01 14:53:18 +0200

    Updates

---
+++ precache-manifest.bee8ed702dae4a9ce83eeceedf0c3f1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45649734c17d6487551d2800166e626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c749570d2769bb7a27ea",
+    "url": "/static/css/main.1726a208.chunk.css"
+  },
+  {
+    "revision": "4aac6aad4b33857b2072",
+    "url": "/static/js/2.675d5ead.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.675d5ead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c749570d2769bb7a27ea",
+    "url": "/static/js/main.91a84437.chunk.js"
+  },
+  {
+    "revision": "4567734cd469a9475f6c",
+    "url": "/static/js/runtime-main.736723a6.js"
+  },
+  {
+    "revision": "6f72f7c118f55ef7218cd7afb64f4d40",
+    "url": "/static/media/profile.6f72f7c1.jpeg"
+  }
+]);
